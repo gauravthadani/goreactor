6fdf48d4fd9fa780ccf70924daec38bbc44a9864
prabhu-seshadri <prabhu.seshadri@gmail.com>
2015-08-04 20:23:41 +0530

    <prabhu> more fixes to the docker splitting

--- build.sh
+++ build.sh
@@ -1,75 +1,73 @@
 #!/usr/bin/env bash
 
 buildServiceImage() {
-	servicename=$1
-	if [ "$servicename" -eq "" ]; then
+	servicename=$1		
+
+	echo "Building Service image"
+
+	if [ "${servicename}" = "" ]; then
 		echo "No service specified"
 		exit
 	fi
 
-	if [ ! -f service/${serivicename}/${serivicename} ]; then
-		echo "Compiling price app"
-		cd service/${serivicename}
-		go build
-		cd ../..
+	#if [ ! -f service/${servicename}/${servicename} ]; then
+		echo "Compiling price app" 
+		echo "Present directory :${PWD}"
+		cd service/${servicename}
+		go build		
 		echo "Done"
-	fi
+		echo "Present directory :${PWD}"
+	#fi
 
-	if [ -f service/${serivicename}/${serivicename} ]; then
+	#if [ -f service/${serivicename}/${serivicename} ]; then
 		echo "Building docker image"
-		docker build -t ${serivicename}_image .	
+		imageName=${servicename}_image
+		echo ${imageName}
+		docker build -t ${imageName} .	
 		echo "Done"
-	fi
+	#fi	
 }
 
 runService () {
 	servicename=$1
 	imageName=${servicename}_image
 
-	if [ "$servicename" -eq "" ]; then
+	echo $imageName
+
+	if [ "$servicename" = "" ]; then
 		echo "No service specified"
 		exit
 	fi
-
-	cd service/${serivicename}
-
-	if [ $1 = "run" ]; then
-
-		echo "Killing any running container"
-		docker rm -f $imageName
-
-		container=${servicename}
-
-		echo "Running ${servicename}"
-		docker run -d -h ${container} --name ${container} $imageName
-
-		echo "Updating dns"
-		new_ip=$(docker inspect ${container} | grep IPAddress | cut -f4 -d'"')
-		sudo container=$conatiner,new_ip=${new_ip}  echo "host-record=${container},${new_ip}" > /opt/docker/dnsmasq.d/0host_$container
-		sudo service dnsmasq restart
-		echo "Done"
-	fi
-}
-
-buildServiceImage price 
-buildServiceImage queryengine
-
-runImage price 
-runImage queryengine
-
-if [ $1 = "run" ]; then
 	
-	echo "Killing any running contaainer"
-	docker rm -f goreactor
+		
+	container=${servicename}
+
+	echo "Killing any running container"
+	docker rm -f ${container}
+
 	
-	echo "Running goreactor"
-	docker run -d -h goreactor --name goreactor goreactor_image
+
+	echo "Running ${servicename}"
+	docker run -d --dns 172.17.42.1 -h ${container} --name ${container} $imageName
 
 	echo "Updating dns"
-	container='goreactor'
-	new_ip=$(docker inspect $container | grep IPAddress | cut -f4 -d'"')
-	sudo container=$conatiner,new_ip=$new_ip  echo "host-record=$container,$new_ip" > /opt/docker/dnsmasq.d/0host_$container
+	new_ip=$(docker inspect ${container} | grep IPAddress | cut -f4 -d'"')
+	sudo container=$conatiner,new_ip=${new_ip}  echo "host-record=${container},${new_ip}" > /opt/docker/dnsmasq.d/0host_$container
 	sudo service dnsmasq restart
 	echo "Done"
+	
+
+	
+}
+
+buildServiceImage "price" 
+buildServiceImage "queryengine" 
+
+
+
+if [ "$1" = "run" ]; then
+runService "price" 
+runService "queryengine" 
+
 fi
 
